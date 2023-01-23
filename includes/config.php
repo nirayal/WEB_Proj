616df6864271c2721d858830e33aba407205a3dd
nirayal <nir.ayal@gmail.com>
2023-01-23 15:34:23 +0200

    helthy food and suggestion finish

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('DB_HOST','localhost');
+    define('DB_HOST', 'localhost');
     define('DB_USER', 'tomerbm_nirayal');
     define('DB_PASS', 'admin');
     define('DB_NAME', 'tomerbm_WEB_Proj');

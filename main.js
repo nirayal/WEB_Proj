f194c9a335c9d7648a7eb1c9bf2df52e34a6f092
nirayal <nir.ayal@gmail.com>
2023-01-23 12:49:20 +0200

    carusel fix done

--- main.js
+++ main.js
@@ -20,45 +20,45 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block"; 
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
     
-    var carousel = document.getElementById("carouselContainer");
-    carousel.addEventListener("touchstart", handleTouchStart, false);        
-    carousel.addEventListener("touchmove", handleTouchMove, false);
+//     var carousel = document.getElementById("carouselContainer");
+//     carousel.addEventListener("touchstart", handleTouchStart, false);        
+//     carousel.addEventListener("touchmove", handleTouchMove, false);
 
-    var xDown = null;
-    var yDown = null;
+//     var xDown = null;
+//     var yDown = null;
 
-    function handleTouchStart(evt) {
-        xDown = evt.touches[0].clientX;
-        yDown = evt.touches[0].clientY;
-    }
+//     function handleTouchStart(evt) {
+//         xDown = evt.touches[0].clientX;
+//         yDown = evt.touches[0].clientY;
+//     }
 
-    function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+//     function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
 
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
+//     var xUp = evt.touches[0].clientX;
+//     var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-        /* left swipe */
-        plusSlides(1);
-        }
-        else {
-        /* right swipe */
-        plusSlides(-1);
-        }                       
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;
-    }
-  });
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 0 ) {
+//         /* left swipe */
+//         plusSlides(1);
+//         }
+//         else {
+//         /* right swipe */
+//         plusSlides(-1);
+//         }                       
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;
+//     }
+//   });
 

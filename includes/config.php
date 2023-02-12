da95561d57429bddcf0c14a20d23d46b2d46bfdb
nirayal <nir.ayal@gmail.com>
2023-02-12 20:07:40 +0200

    config

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,9 @@
     define('DB_USER', 'tomerbm_nirayal');
     define('DB_PASS', 'admin');
     define('DB_NAME', 'tomerbm_web_proj');
+
+    // define('DB_HOST','localhost');
+    // define('DB_USER', 'root');
+    // define('DB_PASS', '');
+    // define('DB_NAME', 'web_proj'); 
 ?>
